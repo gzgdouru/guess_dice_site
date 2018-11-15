f4c0df72c8577635910fe08d66cbb50ce0a6fc4b
ouru <18719091650@163.com>
2018-11-15 15:23:20 +0800

    modify

--- guess_dice/middleware/ipAddress.py
+++ guess_dice/middleware/ipAddress.py
@@ -5,7 +5,12 @@ from analysis.models import ClientIp
 
 class MarkVisitIpMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        remote_ip = request.META["REMOTE_ADDR"]
+        try:
+            realIp = request.META["HTTP_X_FORWARDED_FOR"]
+            print(realIp)
+            realIp = realIp.split(",")[0]
+        except:
+            realIp = request.META["REMOTE_ADDR"]
         url = request.path
-        if remote_ip != "127.0.0.1":
-            ClientIp(ip=remote_ip, url=url).save()
+        if realIp != "127.0.0.1":
+            ClientIp(ip=realIp, url=url).save()
